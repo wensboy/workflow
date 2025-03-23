f6a7c9c871cc3b2a0e5d5c6259df00caf0e71b84
wensboy <wendisx158@163.com>
2025-03-23 21:45:56 +0800

    v0.0.4

--- nvim/lua/config/keybind.lua
+++ nvim/lua/config/keybind.lua
@@ -1,6 +1,14 @@
 -- vim keybindings
+
+-- leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
+-- delete
 vim.keymap.set("n","x",'"_x',{silent = true})
 vim.keymap.set("n","dd",'"_dd',{silent = true})
+
+-- exchange text
+vim.keymap.set("n","<leader>k","yyddkP",{silent = true})
+vim.keymap.set("n","<leader>j","yyddp",{silent = true})
+
