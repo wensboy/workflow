a89075ae0acf5216d64ff6f4b9fcd6abf1920212
wensboy <wendisx158@163.com>
2025-04-15 14:56:22 +0800

    v0.1.1

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -10,6 +10,25 @@ return {
     },
     config = function()
         local cmp = require('cmp')
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            },
+        })
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' }
+                    }
+                }
+            })
+        })
         cmp.setup({
             snippet = {
                 expand = function(args)
