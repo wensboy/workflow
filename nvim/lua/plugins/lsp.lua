619f6dbb29b39db32a484bfb4b8239b91ac2208b
wendisx <wendisx@DESKTOP-B7LPAMN.>
2025-04-20 16:22:42 +0800

    v0.1.2

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,12 +2,51 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
         local lsp = require('lspconfig')
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        local toggle_inlay_hint = function(buf)
+            vim.keymap.set('n','<leader>ih',function()
+                if vim.lsp.inlay_hint then
+                    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({bufnr = buf}),{bufnr = buf})
+                end
+            end,{desc = "toggle inlay_hint status(current buffer)",buffer = bufnr})
+        end
+        
+        local on_attach_go = function(client,bufnr)
+            toggle_inlay_hint(bufnr)
+        end
+
+        local on_attach_rust = function(client,bufnr)
+            toggle_inlay_hint(bufnr)
+        end
+
         lsp.gopls.setup({
             cmd = {"/home/wendisx/go/bin/gopls"},
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            capabilities = capabilities,
+            on_attach = on_attach_go,
+            settings = {
+                gopls = {
+                    hints = {
+                        assignVariableTypes = true,
+                        compositeLiteralFields = true,
+                        parameterNames = true,
+                    }
+                }
+            },
         })
-        lsp.lua_ls.setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        lsp.rust_analyzer.setup({
+            cmd = {"/home/wendisx/.cargo/bin/rust-analyzer"},
+            capabilities = capabilities,
+            on_attach = on_attach_rust,
+            settings = {
+                ["rust-analyzer"] = {
+                   inlayHints = {
+                        parameterNames = {enable = true},
+                        typeHints = {enable = true},
+                        chainingHints = {enable = true},
+                   } 
+                }
+            },
         })
     end
 }
